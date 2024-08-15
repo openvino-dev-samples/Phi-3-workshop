c228563ae0147ccb1aeb29a8f8aa03d3840aeda2
ethan <ethan.yang@intel.com>
2024-08-14 19:57:24 -0700

    notebooks verified
    notebooks verified

--- run_lab.bat
+++ run_lab.bat
@@ -1,5 +1,5 @@
 @echo off
 call "C:\Users\%USERNAME%\miniconda3\Scripts\activate.bat" C:\Users\%USERNAME%\miniconda3\envs\openvino_env
-python src/run_lab.py
+jupyter lab .
 pause
 call "C:\Users\%USERNAME%\miniconda3\Scripts\deactivate.bat"
